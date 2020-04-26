2c9891d7568afa487b9fa44d8d1ed6b893e4e647
Snega <32067368+beloruszhenia@users.noreply.github.com>
2020-04-27 00:11:53 +0300

    done

---
+++ Console24_04/Console24_04/Minmax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console24_04
+{
+    class Minmax
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+
+        public int Srednee()
+        {            
+            return ((Min + Max) / 2 );
+        }
+
+
+    }
+}
--- Console24_04/Console24_04/Program.cs
+++ Console24_04/Console24_04/Program.cs
@@ -4,12 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*создайте пожалуйста новый класс в котором есть два свойства "Min" и "Max" 
+ * (целое число) и метод который получает среднее значение из Min и Max
+ * (результат должен определиться по формуле (Min + Max)/2 )*/
+
 namespace Console24_04
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Minmax p = new Minmax();
+
+            p.Min = 16;
+            p.Max = 66;
+
+            Console.WriteLine("Min = {0}    Max={1}", p.Min, p.Max);
+            Console.WriteLine($"Srednee znachenie = {p.Srednee()}");
+            Console.ReadKey();
         }
     }
 }
